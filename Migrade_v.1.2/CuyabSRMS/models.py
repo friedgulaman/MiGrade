2b2446ebb0c8178250f8301ff6e0cb3cb4f160d3
rinnsagaro123 <143693217+rinnsagaro123@users.noreply.github.com>
2023-11-09 00:35:35 +0800

    11/9/2023 - 12:35am

--- Migrade_v.1.2/CuyabSRMS/models.py
+++ Migrade_v.1.2/CuyabSRMS/models.py
@@ -191,10 +191,7 @@ class Subject(models.Model):
 
 class Quarters(models.Model):
     id = models.AutoField(primary_key=True)
-    first_quarter = models.PositiveIntegerField(default=40)
-    second_quarter = models.PositiveIntegerField(default=40)
-    third_quarter = models.PositiveIntegerField(default=40)
-    fourth_quarter = models.PositiveIntegerField(default=40)
+    quarters = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return self.name
